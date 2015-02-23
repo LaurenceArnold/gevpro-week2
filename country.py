cc46de660e7e8a2f5a4bb0cf1d2382518f14362c
Laurence <l.h.a.arnold@student.rug.nl>
2015-02-23 16:47:29 +0100

    Docstrings toegevoegd + landenlijst

--- country.py
+++ country.py
@@ -1,10 +1,13 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python
+
+"""This class generates a Country which when called can greet you from that country"""
 
 class Country:
     def __init__(self,country):
         self.country=country
 
     def getCountry(self):
+		"""Returns the country"""
         return self.country
 
     def __str__(self):
